86bcce0c97c501ce857ab06ab247c009684a9dd4
redoc <619675912@qq.com>
2020-03-13 14:31:51 +0800

    Update drv_max7219.c

--- drv_max7219.c
+++ drv_max7219.c
@@ -13,7 +13,7 @@
 #include "string.h"
 #include "drv_spi.h"
 
-#define DRV_DEBUG
+//#define DRV_DEBUG
 #define LOG_TAG "drv.max7219"
 #include <drv_log.h>
 
@@ -44,66 +44,100 @@ static void max7219_reg_write(uint8_t cmd, uint8_t value)
     rt_spi_transfer_message(_max7219.spi_device, &msg1);
 }
 
-void max7219_clear(uint8_t dig)
+/**
+ * the Digital tubes blanking.
+ * @note 1 to scan_num.
+ *
+ * @return rt_err_t
+ */
+rt_err_t max7219_clear(uint8_t dig)
 {
     if(dig > (_max7219.info.scan_num))
     {
         log_e("dig num fault.");
-        return;
+        return -RT_ERROR;
     }
 
     max7219_reg_write(dig, 0);
+    return RT_EOK;
 }
 
-void max7219_clear_all(void)
+/**
+ * all the Digital tubes blanking.
+ * @note 1 to scan_num.
+ *
+ * @return rt_err_t
+ */
+rt_err_t max7219_clear_all(void)
 {
     for(uint8_t i = 0; i <=_max7219.info.scan_num; i++)
     {
-        max7219_clear(i);
+        if( -RT_ERROR == max7219_clear(i))
+        {
+            return -RT_ERROR;
+        }
     }
+    return RT_EOK;
 }
 
-void max7219_write_num(uint8_t dig, uint8_t data)
+/**
+ * Write data to max7219.
+ * @note dig must less the scan_num.
+ *
+ * @param dig the dig num(eg.1-->6)
+ * @param data write the dig data(eg.0x7e is num 0)
+ *
+ * @return rt_err_t
+ */
+rt_err_t max7219_write(uint8_t dig, uint8_t data)
 {
-    uint8_t value = 0;
-
     if(dig > (_max7219.info.scan_num))
     {
         log_e("dig num fault.");
-        return;
+        return -RT_ERROR;
     }
+
+    max7219_reg_write(dig, data);
+    return RT_EOK;
+}
+
+/**
+ * Write num to max7219.
+ * @note support 0x0 to 0xf.
+ *
+ * @param dig the dig num(1----scan_num)
+ * @param num write the num
+ *
+ * @return rt_err_t return result
+ */
+rt_err_t max7219_write_num(uint8_t dig, uint8_t num)
+{
+    uint8_t value = 0;
 
     if(value > 0x0f)
     {
         log_e("value falut ,it can't max 0x0f.");
-        return;
+        return -RT_ERROR;
     }
 
-    max7219_reg_write(dig, no_code_buf[data]);
+    max7219_reg_write(dig, no_code_buf[num]);
+    
+    return RT_EOK;
 }
 
-void max7219_write(uint8_t dig, uint8_t data)
-{
-    if(dig > (_max7219.info.scan_num))
-    {
-        log_e("dig num fault.");
-        return;
-    }
-
-    max7219_reg_write(dig, data);
-}
-
-void max7219_intensity_set(uint8_t value)
+rt_err_t max7219_intensity_set(uint8_t value)
 {
     if(value > 0x0f)
     {
         log_e("intnsity value over 0x0f.");
+        return -RT_ERROR;
     }
     else
     {
         _max7219.info.intensity = value;
         max7219_reg_write(REG_ADDR_INTENSITY, value);
     }
+    return RT_EOK;
 }
 
 void max7219_init(void)
@@ -119,49 +153,17 @@ void max7219_init(void)
 
 int drv_max7219_hw_init(void)
 {
-    rt_err_t result = RT_EOK;
     struct rt_spi_configuration cfg = RT_MAX7219_DEFAULT_SPI_CFG;
     struct rt_device_max7219_info info = MAX7219_INFO_DEFAULT;
 
     memset(&_max7219, 0x00, sizeof(_max7219));
-
+    memcpy(&_max7219.info, &info,sizeof(struct rt_device_max7219_info));
+    
     _max7219.spi_device = (struct rt_spi_device *)rt_device_find(MAX7219_SPI_DEVICE_NAME);
+    
     rt_spi_configure(_max7219.spi_device, &cfg);
 
-    memcpy(&_max7219.info, &info,sizeof(struct rt_device_max7219_info));
-
-    return result;
+    return RT_EOK;
 }
 INIT_APP_EXPORT(drv_max7219_hw_init);
 
-
-#if defined(RT_USING_FINSH) && defined(FINSH_USING_MSH)
-#include <finsh.h>
-static void max7219_intensity(uint8_t argc, char **argv)
-{
-	
-#define CMD_INDEX  0
-	
-    size_t i = 0;
-	
-    const char* help_info[] =
-    {
-        [CMD_INDEX]      = "max7219_intensity [0-15]    - set intensity ",
-    };
-    
-    if (argc != 2)
-    {
-        rt_kprintf("Usage:\n");
-        for (i = 0; i < sizeof(help_info) / sizeof(char*); i++)
-        {
-            rt_kprintf("%s\n", help_info[i]);
-        }
-        rt_kprintf("\n");
-    }
-    else
-    {
-        max7219_intensity_set(strtol(argv[1], NULL, 0));
-    }
-}
-MSH_CMD_EXPORT(max7219_intensity, max7219 intensity set);
-#endif /* defined(RT_USING_FINSH) && defined(FINSH_USING_MSH) */
